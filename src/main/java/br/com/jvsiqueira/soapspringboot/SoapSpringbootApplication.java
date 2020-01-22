04fc27fa500db76c71458ac5570ac3c825f20e6f
JonatasVeloso <jvelosos@indracompany.com>
2020-01-22 16:22:53 -0300

    Adicionando XSD

---
+++ src/main/java/br/com/jvsiqueira/soapspringboot/SoapSpringbootApplication.java
@@ -0,0 +1,11 @@
+package br.com.jvsiqueira.soapspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapSpringbootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SoapSpringbootApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/jvsiqueira/soapspringboot/SoapSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jvsiqueira.soapspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
